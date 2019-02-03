c18b60d9f5d49f0133b8ffcea83343c076e94390
Paul Williams <paul@paulswilliams.me.uk>
2019-02-03 17:01:32 +0000

    Empty api project

---
+++ graphql-playground-api/src/main/java/uk/me/paulswilliams/graphqlplaygroundapi/GraphqlPlaygroundApiApplication.java
@@ -0,0 +1,14 @@
+package uk.me.paulswilliams.graphqlplaygroundapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPlaygroundApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlPlaygroundApiApplication.class, args);
+	}
+
+}
+
